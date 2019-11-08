a84883ec9f26b0c904af7da232b3ce51e576ef47
Francisco Lucas <lustparadisestudio@gmail.com>
2019-11-08 13:29:47 -0300

    config heroku port

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(require("./routes"));
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
