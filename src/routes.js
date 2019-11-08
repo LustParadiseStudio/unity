6f5a91aeca4ae946c87f79276b3dc28662ea459b
Francisco Lucas <lustparadisestudio@gmail.com>
2019-11-08 13:20:00 -0300

    add gitignore and routes

--- src/routes.js
+++ src/routes.js
@@ -1,2 +1,11 @@
 const routes = require("express").Router();
+
+routes.get("/hello", function(req, res) {
+  res.send("hello world");
+});
+
+routes.get("/painel", function(req, res) {
+  res.sendfile("public/index.html");
+});
+
 module.exports = routes;
